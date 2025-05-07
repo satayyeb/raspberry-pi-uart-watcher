05c6b761993d58d87f97cb7dade9102e22c961e8
satayyeb <salitayyeb@gmail.com>
2025-05-07 15:35:15 +0330

    working version

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/tarm/serial"
 )
@@ -41,7 +44,7 @@ func main() {
 	}
 	defer port.Close()
 
-	//fmt.Printf("Listening on %s at %d baud...\n", *portName, *baudRate)
+	fmt.Printf("Listening on %s at %d baud... (press Ctrl+C to exit)\n", *portName, *baudRate)
 
 	// Open log file
 	logFile, err := os.OpenFile(*logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -50,16 +53,26 @@ func main() {
 	}
 	defer logFile.Close()
 
+	// Setup signal handling for Ctrl+C
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	// Read lines from UART
 	scanner := bufio.NewScanner(port)
 	for scanner.Scan() {
-		line := scanner.Text()
-		//fmt.Println(line)
+		select {
+		case <-ctx.Done():
+			fmt.Println("\nReceived interrupt signal. Exiting.")
+			return
+		default:
+			line := scanner.Text()
+			fmt.Println(line)
 
-		if containsError(line) {
-			_, err := logFile.WriteString(fmt.Sprintf("Error detected: %s\n", line))
-			if err != nil {
-				log.Printf("Failed to write to log file: %v", err)
+			if containsError(line) {
+				_, err := logFile.WriteString(fmt.Sprintf("Error detected: %s\n", line))
+				if err != nil {
+					log.Printf("Failed to write to log file: %v", err)
+				}
 			}
 		}
 	}
